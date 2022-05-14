f37eff0d86d0ce0b71ae605aab4a480c090bd492
Sammy <segzyfaj1@gmail.com>
2022-05-14 22:50:08 +0100

    A new commit

--- CGRBackEnd/App.js
+++ CGRBackEnd/App.js
@@ -1,26 +1,20 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import exphbs from 'express-handlebars';
-import nodemailer from 'nodemailer';
-
+const express = require("express");
+const dotenv = require("dotenv");
+const nodemailer = require("nodemailer");
 const app = express();
 
-app.engine('handlebars', exphbs());
-app.set('view engines', 'handlebars')
-
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+dotenv.config()
 
 
 
-
-app.get('/', (req, res) => {
+app.post('/form', (req, res) => {
     res.send('Hello from this side')
 })
 
-const port = 3000;
 
-app.listen(port, (req, res) => {
-    console.log('Server has Started')
+const Port = process.env.PORT || 3000;
+
+app.listen(Port, (req, res) => {
+    console.log(`Server is now running on port ${Port}`)
 })
 
