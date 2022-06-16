e5af5ebee5b2d89174f9d8108e1263d33ff01a23
Sammy <segzyfaj1@gmail.com>
2022-06-16 23:48:43 +0100

    A new Commit

--- frontend/src/components/Navigation/Navbar/Navbar.js
+++ frontend/src/components/Navigation/Navbar/Navbar.js
@@ -22,24 +22,9 @@ function Navbar() {
           <div className="navbar-brand">
             <img src={instagram} alt="" />
           </div>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="menu-icon" onClick={handleClick}>
-            <i className={click ? <FaBars /> : <FaTimes />} />
-          </div>
-
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
+      
+          <div >
+            <ul>
               <li className="nav-item">
                 <Link className="nav-link " aria-current="page" to="/">
                   <FontAwesomeIcon icon={faHome} /> HOME
